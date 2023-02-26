3c73963fa899a7f334109dfd6420d8c594396702
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-02-26 15:10:45 +0700

    I think it's better to do not have global variables

--- 0-limit-crawler/main.go
+++ 0-limit-crawler/main.go
@@ -15,11 +15,9 @@ import (
 	"time"
 )
 
-var ticker = time.NewTicker(time.Second)
-
 // Crawl uses `fetcher` from the `mockfetcher.go` file to imitate a
 // real crawler. It crawls until the maximum depth has reached.
-func Crawl(url string, depth int, wg *sync.WaitGroup) {
+func Crawl(url string, depth int, wg *sync.WaitGroup, ticker *time.Ticker) {
 	defer wg.Done()
 
 	if depth <= 0 {
@@ -39,7 +37,7 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 	for _, u := range urls {
 		// Do not remove the `go` keyword, as Crawl() must be
 		// called concurrently
-		go Crawl(u, depth-1, wg)
+		go Crawl(u, depth-1, wg, ticker)
 	}
 	return
 }
@@ -47,7 +45,10 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 func main() {
 	var wg sync.WaitGroup
 
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
 	wg.Add(1)
-	Crawl("http://golang.org/", 4, &wg)
+	Crawl("http://golang.org/", 4, &wg, ticker)
 	wg.Wait()
 }
