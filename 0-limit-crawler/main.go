821e1943c3f13d6ec0b7396000e05a6c9ab4f119
Dmitriy Krasnov <d.krasnov@protonmail.com>
2023-02-26 14:54:24 +0700

    my solution

--- 0-limit-crawler/main.go
+++ 0-limit-crawler/main.go
@@ -12,7 +12,10 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
+
+var ticker = time.NewTicker(time.Second)
 
 // Crawl uses `fetcher` from the `mockfetcher.go` file to imitate a
 // real crawler. It crawls until the maximum depth has reached.
@@ -23,6 +26,7 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 		return
 	}
 
+	<-ticker.C
 	body, urls, err := fetcher.Fetch(url)
 	if err != nil {
 		fmt.Println(err)
